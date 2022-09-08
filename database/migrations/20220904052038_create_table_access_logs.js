37113f39a035da52d5ec4083d9bdce475001ea15
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-09-08 15:25:00 +0800

    Correct fields

--- database/migrations/20220904052038_create_table_access_logs.js
+++ database/migrations/20220904052038_create_table_access_logs.js
@@ -6,35 +6,35 @@ exports.up = function (knex) {
   return knex.schema.createTable("access_logs", function (table) {
     table.bigIncrements("id").primary();
     table.bigInteger("user_id").unsigned().notNullable();
-    table.string("domain", 100).notNullable();
-    table.string("slash_tag", 100).notNullable();
-    table.string("destination", 250).notNullable();
-    table.json("tags").nullable();
-    table.json("channels").nullable();
-    table.string("og_title", 100).defaultTo("");
-    table.string("og_description", 250).defaultTo("");
-    table.string("og_image", 1000).defaultTo("");
-    table.string("utm_source", 100).defaultTo("");
-    table.string("utm_medium", 100).defaultTo("");
-    table.string("utm_campaign", 100).defaultTo("");
-    table.string("utm_term", 100).defaultTo("");
-    table.string("utm_content", 100).defaultTo("");
-    table.tinyint("is_active", 1).defaultTo(0);
-    table.dateTime("starts_at").nullable();
-    table.dateTime("ends_at").nullable();
+    table.bigInteger("link_id").unsigned().notNullable();
+    table.string("link_domain_slash_tag", 200).notNullable();
+    table.string("link_domain", 100).notNullable();
+    table.string("link_utm_source", 100).defaultTo("");
+    table.string("link_utm_medium", 100).defaultTo("");
+    table.string("link_utm_campaign", 100).defaultTo("");
+    table.string("link_utm_term", 100).defaultTo("");
+    table.string("link_utm_content", 100).defaultTo("");
+    table.string("ip", 40).notNullable();
+    table.string("location_country", 100).defaultTo("");
+    table.string("location_continent", 100).defaultTo("");
+    table.text("user_agent").notNullable();
+    table.string("device_type", 25).defaultTo("");
+    table.string("device_browser", 25).defaultTo("");
+    table.string("device_os", 25).defaultTo("");
+    table.string("referrer", 25).defaultTo("");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
-    table.timestamp("deleted_at").nullable();
 
     table
-      .foreign("user_id", "idx_links_user_id")
+      .foreign("user_id", "idx_access_logs_user_id")
       .references("id")
       .inTable("users");
-    table.unique(["domain", "slash_tag"], {
-      indexName: "uniq_links_domain_slash_tag",
-    });
+    table
+      .foreign("link_id", "idx_access_logs_link_id")
+      .references("id")
+      .inTable("links");
 
-    table.engine("MyISAM");
+    table.engine("InnoDB");
     table.charset("utf8mb4");
     table.collate("utf8mb4_general_ci");
   });
