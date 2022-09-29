ccb2313aeb09d3f5ce6f898a6c09e63abc9b4d61
Edgardo Guzman Jr <eegyolk@gmail.com>
2022-09-29 14:32:34 +0800

    Removed starts_at and ends_at field in links table

--- database/migrations/20220904000010_create_table_links.js
+++ database/migrations/20220904000010_create_table_links.js
@@ -22,8 +22,6 @@ exports.up = function (knex) {
     table.string("utm_term", 100).defaultTo("");
     table.string("utm_content", 100).defaultTo("");
     table.tinyint("is_active", 1).defaultTo(0);
-    table.dateTime("starts_at").nullable();
-    table.dateTime("ends_at").nullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
     table.timestamp("deleted_at").nullable();
